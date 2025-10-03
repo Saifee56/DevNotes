7929d9edd6d558b63cf1df6d995570a964f33f9a
saifee56 <safuanalam123@gmail.com>
2025-10-03 22:35:59 +0600

    .

--- blogs/settings.py
+++ blogs/settings.py
@@ -135,4 +135,4 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL='api.CustomUserModel'
 
-ALLOWED_HOSTS = ['devnotes-6y9g.onrender.com']
+ALLOWED_HOSTS = ['*']
